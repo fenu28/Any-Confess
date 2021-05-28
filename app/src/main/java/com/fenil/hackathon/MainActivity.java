81e8ab331648fcaeaeae24ca057d7652fd20d70f
fenu28 <kamdarfenil88@gmail.com>
2021-05-29 02:18:41 +0530

    Implemented Terms of Service dialog in MainActivity.java.

--- app/src/main/java/com/fenil/hackathon/MainActivity.java
+++ app/src/main/java/com/fenil/hackathon/MainActivity.java
@@ -1,12 +1,15 @@
 package com.fenil.hackathon;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
 import android.provider.Settings;
 import android.widget.Toast;
 
@@ -26,13 +29,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        if (getAndroidID().equals("null")) {
+            new Handler().post(new Runnable() {
+                @Override
+                public void run() {
+                    showTermsOfService();
+                }
+            });
+            saveAndroidID();
+            Toast.makeText(MainActivity.this,getAndroidID(),Toast.LENGTH_LONG).show();
+        }
+        else
+            Toast.makeText(MainActivity.this,getAndroidID(),Toast.LENGTH_SHORT).show();
         posts = new ArrayList<>();
         recyclerView = findViewById(R.id.post_recyclerview);
 
-        if(getAndroidID().equals("null"))
-            saveAndroidID();
-        androidID = getAndroidID();
         Toast.makeText(this,androidID,Toast.LENGTH_SHORT).show();
         Post post = new Post("How to build an Android App?","This article is a beginning of something very exciting. Keep checking out this space for updates and news");
 
@@ -48,15 +59,38 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void saveAndroidID() {
-        SharedPreferences sharedPreferences = getSharedPreferences("androidID",MODE_PRIVATE);
+        SharedPreferences sharedPreferences = getSharedPreferences("androidID", MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPreferences.edit();
         editor.putString("androidID", Settings.Secure.getString(this.getContentResolver(),
                 Settings.Secure.ANDROID_ID));
         editor.commit();
     }
-    public String getAndroidID()
-    {
-        SharedPreferences sharedPreferences = getSharedPreferences("androidID",MODE_PRIVATE);
-        return sharedPreferences.getString("androidID","null");
+
+    public String getAndroidID() {
+        SharedPreferences sharedPreferences = getSharedPreferences("androidID", MODE_PRIVATE);
+        return sharedPreferences.getString("androidID", "null");
     }
+
+    public void showTermsOfService() {
+        AlertDialog.Builder termsOfService = new AlertDialog.Builder(this);
+        termsOfService.setMessage("This app does not store your data on any local or cloud storage. Your data remains solely yours and will never get interfered with. Do you agree to our terms of service?");
+        termsOfService.setPositiveButton("Agree",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface arg0, int arg1) {
+
+                    }
+                });
+
+        termsOfService.setNegativeButton("Disagree", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        });
+
+        AlertDialog alertDialog = termsOfService.create();
+        alertDialog.show();
+    }
+
 }
--- app/src/main/java/com/fenil/hackathon/SplashScreen.java
+++ app/src/main/java/com/fenil/hackathon/SplashScreen.java
@@ -1,12 +1,17 @@
 package com.fenil.hackathon;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.provider.Settings;
+import android.view.View;
+import android.widget.Toast;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -14,28 +19,17 @@ public class SplashScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent i = new Intent(SplashScreen.this,MainActivity.class);
+                Intent i = new Intent(SplashScreen.this, MainActivity.class);
                 startActivity(i);
                 finish();
             }
-        },SPLASH_SCREEN_TIME);
+        }, SPLASH_SCREEN_TIME);
     }
 
-    public void saveAndroidID() {
-        SharedPreferences sharedPreferences = getSharedPreferences("androidID",MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.putString("androidID", Settings.Secure.getString(this.getContentResolver(),
-                Settings.Secure.ANDROID_ID));
-        editor.commit();
-    }
-    public String getAndroidID()
-    {
-        SharedPreferences sharedPreferences = getSharedPreferences("androidID",MODE_PRIVATE);
-        return sharedPreferences.getString("androidID","null");
-    }
 }
